d9698d6af8b3f0bf7826385af5bd3429214f66c6
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-11-22 20:04:43 +0100

    Add AIStimulusCallBack

---
+++ Assets/Script/AI/AIStimulusCallBack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Script.AI
+{
+    public class AIStimulusCallBack : MonoBehaviour
+    {
+        public AIStimulus aiStimulus;
+        
+        public UnityEvent<AIStimulusResult> onReceiveStimulus;
+
+        public void Invoke(AIStimulus stimulus, AIStimulusResult stimulusResult)
+        {
+            if (stimulus.name == aiStimulus.name)
+            {
+                onReceiveStimulus?.Invoke(stimulusResult);
+            }
+        }
+    }
+}
