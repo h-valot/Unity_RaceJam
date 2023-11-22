9299711a5c3fb227e0eeef16f7b4654b463ba6e6
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-11-22 19:51:42 +0100

    Add AIStimulus
    See AI documentation for further informations.

---
+++ Assets/Script/AI/AIStimulus.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Script.AI
+{
+    [UnityEngine.CreateAssetMenu(fileName = "NewAIStimulus", menuName = "AI/New AI Stimulus", order = 0)]
+    public class AIStimulus : ScriptableObject
+    {
+        public List<string> tags = new List<string>();
+
+        public bool HaveTag(string tag)
+        {
+            return this.tags.Contains(tag);
+        }
+    }
+}
