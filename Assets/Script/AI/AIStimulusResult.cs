d10454487d107a2a4f9f8288d3015a6f44310fed
eVisualUser <sylvain.tosoni.dev@protonmail.com>
2023-11-22 19:53:05 +0100

    Add AIStimulusResult
    See AI documentation for further information's.

---
+++ Assets/Script/AI/AIStimulusResult.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Script.AI
+{
+    public class AIStimulusResult
+    {
+        public AIStimulus aiStimulus;
+        public Transform sourceTransform;
+        public List<Transform> otherTansforms;
+    }
+}
