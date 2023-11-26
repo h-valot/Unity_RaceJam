d3207a8a99b310a0dd260b275444f3469dc11a82
nobukky <nobukky19@gmail.com>
2023-11-26 20:58:16 +0100

    Add get starting and ending cell

--- Assets/Scripts/Map/Cell.cs
+++ Assets/Scripts/Map/Cell.cs
@@ -3,9 +3,10 @@ public class Cell
     public int x, y; 
     public bool isAvailable;
 
-    public Cell(int x, int y)
+    public Cell(int x, int y, bool isAvailable)
     {
         this.x = x;
         this.y = y;
+        this.isAvailable = isAvailable;
     }
 }
--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -31,10 +31,11 @@ public static class MapGenerator
     {
         for (int x = 0; x < _mapConfig.mapSize; x++)
         {
+            _cells.Add(new List<Cell>());
             for (int y = 0; y < _mapConfig.mapSize; y++)
             {
-                // randomly set their availability
-                _cells[x][y] = new Cell(x, y);
+                bool isAvailable = Random.Range(0f, 1f) <= _mapConfig.availabilityPercentage;
+                _cells[x].Add(new Cell(x, y, isAvailable));
             }
         }
     }
@@ -42,11 +43,50 @@ public static class MapGenerator
     private static List<Cell> GeneratePath()
     {
         // select the starting and the ending cell based on settings in config
+        tryAmount = 0;
+        Cell startingCell = GetStartingCell();
+        tryAmount = 0;
+        Cell endingCell = GetEndingCell();
+        
+        Debug.Log("MAP GENERATOR:" +
+                  $"\r\nStarting cell {startingCell.x}, {startingCell.y} availability {startingCell.isAvailable}" +
+                  $"\r\nEnding cell {endingCell.x}, {endingCell.y} availability {endingCell.isAvailable}");
         
         // generate a* path to get the list of cell from the starting to the ending node
         // the a* must avoid unavailable cells 
         // if it can't find a way thought cells, regenerate cells
 
         return new List<Cell>();
+    }
+
+    private static int tryAmount;
+    private static Cell GetStartingCell()
+    {
+        tryAmount++;
+        if (tryAmount >= _mapConfig.mapSize * _mapConfig.mapSize)
+        {
+            Debug.LogError("MAP GENERATOR: all cells are unavailable");
+            return null;
+        }
+        
+        Cell output = _cells[0 + Mathf.FloorToInt((float)tryAmount / (float)_mapConfig.mapSize)][Random.Range(0, _mapConfig.mapSize - 1)];
+        if (!output.isAvailable) output = GetStartingCell();
+
+        return output;
+    }
+
+    private static Cell GetEndingCell()
+    {
+        tryAmount++;
+        if (tryAmount >= _mapConfig.mapSize * _mapConfig.mapSize)
+        {
+            Debug.LogError("MAP GENERATOR: all cells are unavailable");
+            return null;
+        }
+        
+        Cell output = _cells[_mapConfig.mapSize - 1 - Mathf.FloorToInt((float)tryAmount / (float)_mapConfig.mapSize)][Random.Range(0, _mapConfig.mapSize - 1)];
+        if (!output.isAvailable) output = GetEndingCell();
+
+        return output;
     }
 }
